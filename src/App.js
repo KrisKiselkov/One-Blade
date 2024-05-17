a2b0c0fe121bfa742bf2f44c4953e9dd64de2070
KrisKiselkov <cristian.kiselkov@gmail.com>
2024-05-17 13:38:16 +0300

    Home Base Look

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { Nav } from './content/Nav';
+import { RouterProvider, Route } from 'react-router-dom';
+import { createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import { Home } from './content/Landing/Home';
+import { Landing } from './content/Landing/Landing';
+
+
 
 function App() {
+  const router = createBrowserRouter(createRoutesFromElements(
+    <Route>
+      <Route path='/' element={ <Landing /> }/>
+    </Route>
+      ));
   
+
+  return (
+    <>
+      
+      <RouterProvider router={router}></RouterProvider>
+    
+    </>
+  ); 
 }
 
 export default App;
