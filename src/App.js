ebd0d9648db53efb081160d7f46af657048e815b
KrisKiselkov <cristian.kiselkov@gmail.com>
2024-05-31 14:51:00 +0300

    Removing incorrect file entry for content

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
-import { Nav } from './content/Nav';
 import { RouterProvider, Route } from 'react-router-dom';
 import { createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
-import { Home } from './content/Landing/Home';
-import { Landing } from './content/Landing/Landing';
+import { Landing } from './content/Landing';
 
 
 
